150739bb77505b7d9ae414a7c81b0287bdfcdd51
lessly-github <71749962+lessly-github@users.noreply.github.com>
2020-10-10 10:49:59 +0530

    Initial Commit for Docker and Jenkins integration.

--- HelloWorld/src/com/lessly/HelloWorld.java
+++ HelloWorld/src/com/lessly/HelloWorld.java
@@ -1,10 +1,9 @@
 package com.lessly;
 
+
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to Java Project...." + new java.util.Date().toString());
-
 	}
-
 }
